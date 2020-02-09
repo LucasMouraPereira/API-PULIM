34cc46e6b77851e187dcdc413c910a38acfbc4ff
Lucas Moura Pereira <lucasmourapereira@MacBook-Pro-de-Lucas-2.local>
2020-02-09 13:13:31 -0300

    config port

--- src/app.js
+++ src/app.js
@@ -43,7 +43,7 @@ app.use('/institutes', institutesRoute);
 app.use('/clients', clientsRoute);
 app.use('/visits', visitsRoute);
 
-app.listen(8080, () => {
+app.listen(process.env.PORT || 8080, () => {
     console.log('Servidor esta rodando');
 });
 
